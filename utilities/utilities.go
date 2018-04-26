8f883a04e968e3494164ec4661016d86c2d14413
peddlrph <37572697+peddlrph@users.noreply.github.com>
2018-04-26 20:02:49 +0800

    support Float64

--- utilities/utilities.go
+++ utilities/utilities.go
@@ -11,6 +11,10 @@ func DisplayPrettyFloat(f float32) string {
 	return fmt.Sprintf("%.2f", f)
 }
 
+func DisplayPrettyFloat64(f float64) string {
+	return fmt.Sprintf("%.2f", f)
+}
+
 func DisplayPrettyNullFloat64(sqlf sql.NullFloat64) string {
 	//return "Hello"
 	return fmt.Sprintf("%.2f", sqlf.Float64)
@@ -18,5 +22,10 @@ func DisplayPrettyNullFloat64(sqlf sql.NullFloat64) string {
 
 func IsNumeric(s string) bool {
 	_, err := strconv.ParseFloat(s, 64)
+	return err == nil
+}
+
+func IsPositiveInteger(s string) bool {
+	_, err := strconv.ParseUint(s, 10, 64)
 	return err == nil
 }
